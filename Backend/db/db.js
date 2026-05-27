050d5dfa078d4dc4c8ceacdafe3715f1b5d73459
PG1611Bcode <pranaygupta16june@gmail.com>
2026-05-27 12:15:17 +0530

    Fix serverless MongoDB connection

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-function connectToDb(){
-    mongoose.connect(process.env.DB_CONNECT).then(()=>{
-        console.log('Connected to MongoDB')
-}).catch(err => console.log(err));
+async function connectToDb() {
+    if (mongoose.connection.readyState === 1) {
+        console.log('Using existing MongoDB connection');
+        return mongoose.connection;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.DB_CONNECT);
+        console.log('Connected to MongoDB');
+        return db;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
 }
-
 
 module.exports = connectToDb;
